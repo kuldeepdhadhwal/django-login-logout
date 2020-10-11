574708f7d11c985be5ad365e3cda53300eed6e73
Kuldeep Dhadhwal <32818714+kuldeepdhadhwal@users.noreply.github.com>
2020-10-11 12:34:52 +0530

    first commit

---
+++ user_login_and_register/user/models.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=200, help_text="Name of the sender")
+    email = models.EmailField(max_length=200)
+    subject = models.CharField(max_length=200)
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "Contact"
+
+    def __str__(self):
+        return self.name + "-" +  self.email
